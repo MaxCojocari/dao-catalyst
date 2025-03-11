b61c448105e3ece21418bf36b6a217a744139f3b
Maxim Cojocari <cojocarigoncearmax@gmail.com>
2025-03-11 23:42:09 +0200

    implement core dao interface

--- blockchain/contracts/DaoCatalyst.sol
+++ blockchain/contracts/DaoCatalyst.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-contract DaoCatalyst {
-    constructor() {}
-}
+import {IDaoCatalyst} from "./interfaces/IDaoCatalyst.sol";
+
+contract DaoCatalyst is IDaoCatalyst {}
---
+++ blockchain/contracts/interfaces/IDaoCatalyst.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+interface IDaoCatalystErrors {
+    error UnknownProposal(uint256 proposalId);
+}
+
+interface IDaoCatalyst is IDaoCatalystErrors{
+    enum ProposalState {
+        Pending,
+        Active,
+        Canceled,
+        Defeated,
+        Succeeded,
+        Queued,
+        Expired,
+        Executed
+    }
+
+    struct Proposal {
+        uint64 voteStart;
+        uint64 voteEnd;
+        bool executed;
+        bool canceled;
+        address proposer;
+    }
+
+    struct ProposalAction {
+        address targets,
+        uint256 values,
+        bytes calldatas, 
+    }
+
+    event ProposalCreated(
+        uint256 proposalId,
+        address proposer,
+        address[] targets,
+        uint256[] values,
+        string[] signatures,
+        bytes[] calldatas,
+        uint256 voteStart,
+        uint256 voteEnd,
+        string description
+    );
+
+    event ProposalCanceled(uint256 proposalId);
+
+    event ProposalExecuted(uint256 proposalId);
+
+    event VoteCast(address indexed voter, uint256 proposalId, uint8 support, uint256 weight);
+
+    function propose(
+        ProposalAction[] calldata actions
+        string memory description
+    ) external;
+
+    function execute(uint256 proposalId) external payable;
+
+    function state(uint256 proposalId) external view returns (ProposalState);
+
+    function cancel(uint256 proposalId) public virtual returns (uint256 proposalId);
+
+    function castVote(uint256 proposalId, uint8 support) public virtual returns (uint256 balance);
+
+    function proposalProposer(uint256 proposalId) external view returns (address);
+
+    function votingDelay() external view returns (uint256);
+
+    function votingPeriod() external view returns (uint256);
+
+    function quorum(uint256 timepoint) external view returns (uint256);
+
+    function getVotes(address account, uint256 timepoint) external view returns (uint256);
+
+    function hasVoted(uint256 proposalId, address account) external view returns (bool);
+}
