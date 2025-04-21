e56918267fdd4d71cb54f080878e95a240fbd526
Maxim Cojocari <cojocarigoncearmax@gmail.com>
2025-04-21 21:51:11 +0300

    add info section layout

--- frontend/src/components/proposal-details/voting.tsx
+++ frontend/src/components/proposal-details/voting.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { Container } from "../common-styles";
 import { useState } from "react";
-import { ToggleTabs } from "..";
+import { InfoSection, ToggleTabs } from "..";
+import { DaoType } from "../../types";
 
 export const Voting = () => {
   const [activeTab, setActiveTab] = useState("Info");
@@ -12,6 +13,16 @@ export const Voting = () => {
         <h1>Voting</h1>
         <ToggleTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       </Row>
+      <InfoSection
+        daoType={DaoType.SimpleVote}
+        support="> 66%"
+        quorum="≥ 1950 of 13k PIK (15%)"
+        participation="3000 of 13k PIK (23.08%)"
+        participationReached={true}
+        uniqueVoters={4}
+        start="2025/02/05 10:20 PM UTC+2"
+        end="2025/02/06 10:20 PM UTC+2"
+      />
     </Container>
   );
 };
@@ -21,4 +32,5 @@ export const Row = styled.div`
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
+  margin-bottom: 12px;
 `;
