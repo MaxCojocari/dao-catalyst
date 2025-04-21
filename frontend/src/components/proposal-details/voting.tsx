0a871b686e1db9d8de236ecd3dd1b5c37d379fe6
Maxim Cojocari <cojocarigoncearmax@gmail.com>
2025-04-21 22:19:40 +0300

    add vote button

--- frontend/src/components/proposal-details/voting.tsx
+++ frontend/src/components/proposal-details/voting.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Container } from "../common-styles";
+import { Container, NextStepButton } from "../common-styles";
 import { useState } from "react";
 import { InfoSection, ToggleTabs } from "..";
 import { DaoType } from "../../types";
@@ -23,6 +23,7 @@ export const Voting = () => {
         start="2025/02/05 10:20 PM UTC+2"
         end="2025/02/06 10:20 PM UTC+2"
       />
+      <VoteButton disabled={false}>Vote Now</VoteButton>
     </Container>
   );
 };
@@ -33,4 +34,26 @@ export const Row = styled.div`
   justify-content: space-between;
   width: 100%;
   margin-bottom: 12px;
+`;
+
+export const VoteButton = styled(NextStepButton)`
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 32px;
+  gap: 8px;
+
+  width: auto;
+  height: 47px;
+
+  border-radius: 6px;
+
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: -0.02em;
+  color: #ffffff;
 `;
