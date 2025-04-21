a1b1c7cc41bd19655ff0bce267f9bdcaf826954c
Maxim Cojocari <cojocarigoncearmax@gmail.com>
2025-04-21 23:03:11 +0300

    implement vote breakdown section

---
+++ frontend/src/components/proposal-details/voters-section.tsx
@@ -0,0 +1,3 @@
+export const VotersSection = () => {
+  return <>Voters section</>;
+};
--- frontend/src/components/proposal-details/voting.tsx
+++ frontend/src/components/proposal-details/voting.tsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 import { Container, NextStepButton } from "../common-styles";
 import { useState } from "react";
-import { InfoSection, ToggleTabs } from "..";
+import { BreakdownSection, InfoSection, ToggleTabs, VotersSection } from "..";
 import { DaoType } from "../../types";
+import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
+
+export enum VotingTab {
+  Breakdown = "Breakdown",
+  Voters = "Voters",
+  Info = "Info",
+}
 
 export const Voting = () => {
-  const [activeTab, setActiveTab] = useState("Info");
+  const [activeTab, setActiveTab] = useState(VotingTab.Info);
 
   return (
     <Container>
@@ -13,16 +20,34 @@ export const Voting = () => {
         <h1>Voting</h1>
         <ToggleTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       </Row>
-      <InfoSection
-        daoType={DaoType.SimpleVote}
-        support="> 66%"
-        quorum="≥ 1950 of 13k PIK (15%)"
-        participation="3000 of 13k PIK (23.08%)"
-        participationReached={true}
-        uniqueVoters={4}
-        start="2025/02/05 10:20 PM UTC+2"
-        end="2025/02/06 10:20 PM UTC+2"
-      />
+      <VotingStatus>
+        <InfoOutlineIcon
+          sx={{ color: "#6666FF", width: "18px", marginRight: "4px" }}
+        />
+        <p>Voting ends in 59 minutes</p>
+      </VotingStatus>
+      {activeTab === VotingTab.Info && (
+        <InfoSection
+          daoType={DaoType.SimpleVote}
+          support="> 66%"
+          quorum="≥ 1950 of 13k PIK (15%)"
+          participation="3000 of 13k PIK (23.08%)"
+          participationReached={true}
+          uniqueVoters={4}
+          start="2025/02/05 10:20 PM UTC+2"
+          end="2025/02/06 10:20 PM UTC+2"
+        />
+      )}
+      {activeTab === VotingTab.Breakdown && (
+        <BreakdownSection
+          votes={{
+            yes: { amount: "12k PIK", percentage: 52.31 },
+            no: { amount: "1000 PIK", percentage: 7.69 },
+            abstain: { amount: "0 PIK", percentage: 40 },
+          }}
+        />
+      )}
+      {activeTab === VotingTab.Voters && <VotersSection />}
       <VoteButton disabled={false}>Vote Now</VoteButton>
     </Container>
   );
@@ -33,7 +58,22 @@ export const Row = styled.div`
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
+  margin-bottom: 6px;
+`;
+
+export const VotingStatus = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   margin-bottom: 12px;
+
+  p {
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 22px;
+    letter-spacing: -0.02em;
+    color: #6666ff;
+  }
 `;
 
 export const VoteButton = styled(NextStepButton)`
